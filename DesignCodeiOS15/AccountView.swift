333bb9fa709f9bcf86e3e9c2ce3f1dbb8d86c463
Austin Burke <austinlayneburke@gmail.com>
2021-12-31 10:36:26 -0600

    Added customized sf symbol

--- DesignCodeiOS15/AccountView.swift
+++ DesignCodeiOS15/AccountView.swift
@@ -11,6 +11,23 @@ struct AccountView: View {
     var body: some View {
         NavigationView {
             List {
+                VStack {
+                    Image(systemName: "person.crop.circle.badge.checkmark")
+                        .symbolVariant(.circle.fill)
+                        .font(.system(size: 32))
+                        .symbolRenderingMode(.palette)
+                        .foregroundStyle(.blue, .blue.opacity(0.3))
+                        .padding()
+                        .background(Circle().fill(.ultraThinMaterial))
+                        .background(
+                            Image(systemName: "hexagon")
+                                .symbolVariant(.fill)
+                                .foregroundColor(.blue)
+                                .font(.system(size: 200))
+                                .offset(x: -50, y: -100)
+                    )
+                }
+                .frame(maxWidth: .infinity)
                 Section {
                     Text("Settings")
                     Text("Billing")
