289b96cbd8e454b79cfede8e0724cb83dee32a42
Austin Burke <austinlayneburke@gmail.com>
2022-01-03 14:29:01 -0600

    Added TabBar view

--- DesignCodeiOS15/TabBar.swift
+++ DesignCodeiOS15/TabBar.swift
@@ -9,21 +9,37 @@ import SwiftUI
 
 struct TabBar: View {
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottom) {
             ContentView()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .offset(y: 300)
             HStack {
                 Spacer()
-                VStack {
+                VStack(spacing: 0) {
                     Image(systemName: "house")
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 80, height: 29)
                     Text("Learn Now")
+                        .font(.caption2)
                 }
                 Spacer()
-                VStack {
+                VStack(spacing: 0) {
                     Image(systemName: "magnifyingglass")
+                        .symbolVariant(.fill)
+                        .font(.body.bold())
+                        .frame(width: 80, height: 29)
                     Text("Explore")
+                        .font(.caption2)
                 }
                 Spacer()
             }
+            .padding(.top, 14)
+            .frame(height: 88, alignment: .top)
+            .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .strokeStyle(cornerRadius: 34)
+            .frame(maxHeight: .infinity, alignment: .bottom)
+            .ignoresSafeArea()
         }
     }
 }
