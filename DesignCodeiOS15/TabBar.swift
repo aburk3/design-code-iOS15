d3dc6826ca19ea6860f8e7d65d38d77dfb49f510
Austin Burke <austinlayneburke@gmail.com>
2022-01-05 07:03:56 -0600

    Added background animation for tabbar

--- DesignCodeiOS15/TabBar.swift
+++ DesignCodeiOS15/TabBar.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct TabBar: View {
     @State var selectedTab: Tab = .home
+    @State var color: Color = .teal
+    @State var tabItemWidth: CGFloat = 0
     
     var body: some View {
         ZStack(alignment: .bottom) {
@@ -31,7 +33,10 @@ struct TabBar: View {
             HStack {
                 ForEach(tabItems) { item in
                     Button {
-                        selectedTab = item.tab
+                        withAnimation(.spring(response: 0.3, dampingFraction: 0.7)) {
+                            selectedTab = item.tab
+                            color = item.color
+                        }
                     } label: {
                         VStack(spacing: 0) {
                             Image(systemName: item.icon)
@@ -45,12 +50,63 @@ struct TabBar: View {
                         .frame(maxWidth: .infinity)
                     }
                     .foregroundStyle(selectedTab == item.tab ? .primary : .secondary)
+                    .blendMode(selectedTab == item.tab ? .overlay : .normal)
+                    .overlay(
+                        GeometryReader { proxy in
+                            //Text("\(proxy.size.width)")
+                            Color.clear.preference(key: TabPreferenceKey.self, value: proxy.size.width)
+                        }
+                    )
+                    .onPreferenceChange(TabPreferenceKey.self) { value in
+                        tabItemWidth = value
+                    }
                 }
             } //: HSTACK
             .padding(.horizontal, 8)
             .padding(.top, 14)
             .frame(height: 88, alignment: .top)
             .background(.ultraThinMaterial, in: RoundedRectangle(cornerRadius: 34, style: .continuous))
+            .background(
+                HStack {
+                    if selectedTab == .library { Spacer() }
+                    if selectedTab == .explore { Spacer() }
+                    if selectedTab == .notifications {
+                        Spacer()
+                        Spacer()
+                    }
+                    Circle().fill(color).frame(width: tabItemWidth)
+                    if selectedTab == .home { Spacer() }
+                    if selectedTab == .explore {
+                        Spacer()
+                        Spacer()
+                    }
+                    if selectedTab == .notifications { Spacer() }
+                }
+                .padding(.horizontal, 8)
+            )
+            .overlay(
+                HStack {
+                    if selectedTab == .library { Spacer() }
+                    if selectedTab == .explore { Spacer() }
+                    if selectedTab == .notifications {
+                        Spacer()
+                        Spacer()
+                    }
+                    Rectangle()
+                        .fill(color)
+                        .frame(width: 28, height: 5)
+                        .cornerRadius(3)
+                        .frame(width: tabItemWidth)
+                        .frame(maxHeight: .infinity, alignment: .top)
+                    if selectedTab == .home { Spacer() }
+                    if selectedTab == .explore {
+                        Spacer()
+                        Spacer()
+                    }
+                    if selectedTab == .notifications { Spacer() }
+                }
+                .padding(.horizontal, 8)
+            )
             .strokeStyle(cornerRadius: 34)
             .frame(maxHeight: .infinity, alignment: .bottom)
             .ignoresSafeArea()
@@ -61,5 +117,6 @@ struct TabBar: View {
 struct TabBar_Previews: PreviewProvider {
     static var previews: some View {
         TabBar()
+.previewInterfaceOrientation(.landscapeLeft)
     }
 }
