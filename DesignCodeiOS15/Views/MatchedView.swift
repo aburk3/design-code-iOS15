21c1f2e22b71f4f6ca5db9a4433b845b157f1a1b
Austin Burke <austinlayneburke@gmail.com>
2022-01-14 06:31:45 -0600

    Animating two elements with matchedGeometry

--- DesignCodeiOS15/Views/MatchedView.swift
+++ DesignCodeiOS15/Views/MatchedView.swift
@@ -14,13 +14,25 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                Text("SwiftUI")
-                    .matchedGeometryEffect(id: "title", in: namespace)
+                VStack {
+                    Text("SwiftUI")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
                     .frame(maxWidth: .infinity, alignment: .leading)
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.weight(.semibold))
+                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+                }
             } else {
-                Text("SwiftUI")
-                    .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .trailing)
+                VStack {
+                    Text("20 sections - 3 hours".uppercased())
+                        .font(.footnote.weight(.semibold))
+                        .matchedGeometryEffect(id: "subtitle", in: namespace)
+                    Text("SwiftUI")
+                        .font(.largeTitle.weight(.bold))
+                        .matchedGeometryEffect(id: "title", in: namespace)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                }
             }
         }
         .onTapGesture {
