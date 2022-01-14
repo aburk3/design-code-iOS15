eca642127bcfc56368fc03a354c7a32cf3b3b098
Austin Burke <austinlayneburke@gmail.com>
2022-01-14 06:40:56 -0600

    Adding background colors and another text element

--- DesignCodeiOS15/Views/MatchedView.swift
+++ DesignCodeiOS15/Views/MatchedView.swift
@@ -14,25 +14,45 @@ struct MatchedView: View {
     var body: some View {
         ZStack {
             if !show {
-                VStack {
+                VStack(alignment: .leading, spacing: 12) {
                     Text("SwiftUI")
                         .font(.largeTitle.weight(.bold))
+                        // Integrity should be done before matchedGeometryEffect
+                        // modifier, positioning should be done after.
                         .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                     Text("20 sections - 3 hours".uppercased())
                         .font(.footnote.weight(.semibold))
                         .matchedGeometryEffect(id: "subtitle", in: namespace)
+                    Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                        .font(.footnote)
+                        .matchedGeometryEffect(id: "text", in: namespace)
                 }
+                .padding(20)
+                .foregroundStyle(.white)
+                .background(
+                    Color.red.matchedGeometryEffect(id: "background", in: namespace)
+                )
+                .padding(20)
             } else {
-                VStack {
+                VStack(alignment: .leading, spacing: 12) {
+                    Spacer()
+                    Text("Build an iOS app for iOS 15 with custom layouts, animations and ...")
+                        .font(.footnote)
+                        .matchedGeometryEffect(id: "text", in: namespace)
                     Text("20 sections - 3 hours".uppercased())
                         .font(.footnote.weight(.semibold))
                         .matchedGeometryEffect(id: "subtitle", in: namespace)
                     Text("SwiftUI")
                         .font(.largeTitle.weight(.bold))
                         .matchedGeometryEffect(id: "title", in: namespace)
-                    .frame(maxWidth: .infinity, alignment: .leading)
+                        .frame(maxWidth: .infinity, alignment: .leading)
                 }
+                .padding(20)
+                .foregroundStyle(.black)
+                .background(
+                    Color.blue.matchedGeometryEffect(id: "background", in: namespace)
+                )
             }
         }
         .onTapGesture {
